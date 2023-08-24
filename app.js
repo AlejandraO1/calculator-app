02f506d9786ca8ab8c2d63fbbb74295ebcdabe9a
AlejandraO1 <alejandraortegax@gmail.com>
2023-08-24 12:29:09 -0400

    Added calculator class and started implementing numbers being updated onto the calculator screen

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
+class Calculator {
+  constructor(previousEquation, currentEquation) {
+    this.previousEquation = previousEquation;
+    this.currentEquation = currentEquation;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = ``;
+    this.previousOperand = ``;
+    this.operation = undefined;
+  }
+
+  delete() {}
+
+  appendNumber(number) {
+    if (number === `.` && this.currentOperand.includes(`.`)) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = ``;
+  }
+
+  compute() {}
+
+  updateDisplay() {
+    this.currentEquation.innerText = this.currentOperand;
+    this.previousEquation.innerText = this.previousOperand;
+  }
+}
+
 const previousEquation = document.querySelector(`#previous-equation`);
 const currentEquation = document.querySelector(`#current-equation`);
-const numberButtons = document.querySelectorAll(`#number-button`);
-const operatorButtons = document.querySelector(`#operator-button`);
+const numberButtons = document.querySelectorAll(`.number-button`);
+const operatorButtons = document.querySelectorAll(`.operator-button`);
 const clearButton = document.querySelector(`#clear-button`);
 const deleteButton = document.querySelector(`#delete-button`);
-const decimalButton = document.querySelector(`#decimal-button`);
 const equalButton = document.querySelector(`#equal-button`);
+
+const calculator = new Calculator(previousEquation, currentEquation);
+
+numberButtons.forEach((button) => {
+  button.addEventListener(`click`, () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener(`click`, () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
 function add(a, b) {
   return a + b;
